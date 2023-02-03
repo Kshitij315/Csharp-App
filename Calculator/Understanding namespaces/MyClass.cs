f1a0342ccd262d49fb190675e071a40d854dd2df
kshitij khandkar <kshitij311@outlook.com>
2023-02-03 15:34:56 +0530

    using alias to include namespaces

--- Calculator/Understanding namespaces/MyClass.cs
+++ Calculator/Understanding namespaces/MyClass.cs
@@ -4,7 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using ProjectA.TeamA;  //using directive         
+///<summary>
+///namespaces are used to organize your code into different chunks
+///these chunks can be accessed using the name of that namespace
+///microsoft also uses namepsaces like System to organize c# code
+///Or we can add preprocessing directive to include that namespace
+/// </summary>
+/// <summary>
+/// using alias
+/// </summary>
+using Pata=ProjectA.TeamA;  //using alias
+using Patb=ProjectA.TeamB;  //using alias
 namespace Calculator.Understanding_namespaces
 {
     class Program
@@ -12,7 +22,9 @@ namespace Calculator.Understanding_namespaces
         public static void Main()
         {
             // ProjectA.TeamA.classA.Print();
-            classA.Print();
+            //ProjectA.TeamB.classB.Print();
+            Pata.classA.Print();//aliases are used to resolve ambiguity bw names
+            Patb.classB.Print();//same as above
         }
     }
     
@@ -44,3 +56,4 @@ namespace ProjectA
             }
         }
     }
+}
