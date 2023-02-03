c62876a4c497295c37d1709281536f787a6d50e2
kshitij khandkar <kshitij311@outlook.com>
2023-02-03 15:30:02 +0530

    Undestanding namespaces

---
+++ Calculator/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+     class Person
+    {
+        private string Name;//field
+        public string publicName // property
+        {
+            get
+            {
+                if (Name != null)
+                {
+                    return Name;
+                }
+                return null;
+            }
+            set
+            {
+                Name = value; 
+            }
+        }
+        
+        
+
+
+    }
+}
---
+++ Calculator/Understanding namespaces/MyClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectA.TeamA;  //using directive         
+namespace Calculator.Understanding_namespaces
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // ProjectA.TeamA.classA.Print();
+            classA.Print();
+        }
+    }
+    
+
+
+}
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class classA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print method");
+            }
+        }
+    }
+}
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        class classB
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print method");
+            }
+        }
+    }
