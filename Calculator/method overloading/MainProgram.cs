d3a529fcae71db6b1a403f5db3253d79e48b5287
kshitij khandkar <kshitij311@outlook.com>
2023-02-04 12:29:01 +0530

    final commit

--- Calculator/method overloading/MainProgram.cs
+++ Calculator/method overloading/MainProgram.cs
@@ -11,7 +11,7 @@ namespace Calculator.method_overloading
     {
         public static void Main()
         {
-            BaseClass bc=new DerivedClass();
+              BaseClass bc=new DerivedClass();
 
             bc.Print();
         }
